a06eb57f0f5753728ef8c5d9681398787482aa95
dicky.arinal <dicky.arinal@gmail.com>
2015-08-15 23:39:46 +0700

    ADO.NET, EF introduction

---
+++ LamedhPos.UI.Konsole/ProgramEF.cs
@@ -0,0 +1,30 @@
+﻿using LamedhPos.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamedhPos.UI.Konsole
+{
+    public class LamedhPosContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public LamedhPosContext() : base("Server=.\\SQLEXPRESS;Database=LamedhPos;Integrated Security=SSPI")
+        {
+        }
+    }
+
+    class ProgramEF
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new LamedhPosContext();
+            var query = ctx.Employees.Where(e => e.Name.Contains("Su"));
+            foreach (var e in query)
+                Console.WriteLine(e.Name);
+        }
+    }
+}
